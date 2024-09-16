241423b18b13da63e5b2d800a32cacf88718a01a
ernooljakka <61537883+ernooljakka@users.noreply.github.com>
2024-09-16 17:43:15 +0300

    initial frontend setup (#7)
    Co-authored-by: Erno Oljakka <erno.oljakka@tuni.fi>

---
+++ frontend/src/views/event_view.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const event_view = () => {
+
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default event_view;
---
+++ frontend/src/views/move_view.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const move_view = () => {
+
+  return (
+    <div>
+      <h1>Move Device</h1>
+    </div>
+  );
+};
+
+export default move_view;
