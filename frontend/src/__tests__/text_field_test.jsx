23ec13a432b0a2c5067e263a4341d6d1661384e9
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-09-28 02:37:59 +0300

    changed text_field to use PropType

--- frontend/src/__tests__/text_field_test.jsx
+++ frontend/src/__tests__/text_field_test.jsx
@@ -4,13 +4,13 @@ import Text_field from '../components/shared/text_field';
 
 describe('Text_field', () => {
     test('renders with righ label', () => {
-        render(<Text_field label="test-name" />);
+        render(<Text_field label="test-name" value="test-value" onChange={() => {}} />);
         const label = screen.getByLabelText('test-name');
         expect(label).toBeInTheDocument();
     })
     
     test('sets value right', () => {
-        render(<Text_field label="test-name" value="test-value" />);
+        render(<Text_field label="test-name" value="test-value" onChange={() => {}} />);
         const value = screen.getByDisplayValue('test-value');
         expect(value).toBeInTheDocument();
     })
--- frontend/src/components/shared/text_field.jsx
+++ frontend/src/components/shared/text_field.jsx
@@ -1,11 +1,12 @@
 import TextField from '@mui/material/TextField';
+import PropTypes from 'prop-types';
 
 const Text_field = ({
-  label = 'Enter',
+  label,
   value,
   onChange,
-  variant = 'outlined',
-  required = false
+  variant,
+  required = false  //If we need to indicate that field is required
 }) => {
 
 
@@ -14,12 +15,19 @@ const Text_field = ({
         label={label}
         value={value}
         onChange={onChange}
-        variant={variant}
-        required = {required}
+        variant="outlined"
+        required = {required} 
       
       />
           
     );
   };
+  Text_field.propTypes = {
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    required: PropTypes.bool,
+  };
+
   
   export default Text_field;
