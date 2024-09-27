8556d4c40c273272298c28bc33347f1dd7625edf
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-09-28 01:27:35 +0300

    Implemented text_field.jsx and text_field_test.jsx

--- frontend/src/components/shared/text_field.jsx
+++ frontend/src/components/shared/text_field.jsx
@@ -1,11 +1,21 @@
 import TextField from '@mui/material/TextField';
 
-const Text_field = ({}) => {
+const Text_field = ({
+  label = 'Enter',
+  value,
+  onChange,
+  variant = 'outlined',
+  required = false
+}) => {
 
 
     return (
       <TextField
-      
+        label={label}
+        value={value}
+        onChange={onChange}
+        variant={variant}
+        required = {required}
       
       />
           
