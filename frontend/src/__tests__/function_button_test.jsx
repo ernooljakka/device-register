8fbfb15b00cf0ec7b0712b184e580f9f81fd9476
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-09-28 20:50:50 +0300

    implemented function_button and unit tests for it

---
+++ frontend/src/__tests__/function_button_test.jsx
@@ -0,0 +1,21 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/';
+import Function_button from '../components/shared/function_button';
+
+describe('Text_field', () => {
+    test('renders with correct text', () => {
+        render(<Function_button text="test-text" onClick={() => {}} />);
+        const button = screen.getByRole('button');
+        expect(button).toHaveTextContent('test-text');
+    })
+    
+    test('onclick works when clicked', () => {
+        const handleClick = jest.fn();
+        render(<Function_button text='test-text' onClick={handleClick} />);
+        const button = screen.getByRole('button');
+
+        //Simulates a click
+        fireEvent.click(button);
+        expect(handleClick).toHaveBeenCalledTimes(1);
+    })
+})
