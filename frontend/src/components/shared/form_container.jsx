581f86080bfb2fc209d517602b86bbbda3e23dfe
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-09-29 00:46:53 +0300

    removed form_container tests from this ticket

--- frontend/src/components/shared/form_container.jsx
+++ frontend/src/components/shared/form_container.jsx
@@ -1,56 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Function_button from './function_button';
-const Form_container = ({children,
-                          onSubmit,
-                          sx = {},
-                          cardSx = {},
-                          childrenSx = {} }) => {
+
+const Form_container = () => {
 
     return (
-      <Box sx={{ ...defaultSx, ...sx}}>
-      {/*defaultSx is overwritten by sx if that is provided*/}
-        <Card sx = {{ ...defaultCardSx, ...cardSx}}>
-          <CardContent>
-            <form onSubmit = {onSubmit} style={{ ...defaultChildrenSx, ...childrenSx  }}>
-              {children}
-            </form>
-          </CardContent>
-        </Card>
-      </Box>
+      <form>
+        
+      </form>
     );
   };
 
-const defaultSx = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',   
-  padding: 1
 
-}
-
-const defaultCardSx = {
-  width: '100%',
-  maxWidth: 500,
-  padding: 1
-}
-
-const defaultChildrenSx = {
-  display: 'flex',
-  flexDirection: 'column',
-  gap: 50,
-  width: '100%'
-}
-
-Function_button.propTypes = {
-  children: PropTypes.node.isRequired, //mandatory
-  onSubmit: PropTypes.func.isRequired, //mandatory
-  sx: PropTypes.object,
-  cardSx: PropTypes.object
-}
   
 export default Form_container;
--- frontend/src/views/move_view.jsx
+++ frontend/src/views/move_view.jsx
@@ -6,38 +6,9 @@ import Text_field from '../components/shared/text_field'
 
 const Move_view = () => {
 
-  const [formData, setFormData] = useState({ Name: '', Email: '' });
-
-  const handleChange = (e) => {
-    const { label, value } = e.target;
-    console.log(`Field: ${label}, Value: ${value}`);
-    setFormData({
-      ...formData,
-      [label]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form Submitted:', formData);
-  };
-
   return (
     <div>
       <h1>Move Device</h1>
-      <Form_container onSubmit={handleSubmit} cardSx={{ maxWidth: 600, boxShadow: 5 }}>
-      <Text_field
-        label="Name"
-        value={formData.Name}
-        onChange={handleChange}
-      />
-      <Text_field
-        label="Email"
-        value={formData.Email}
-        onChange={handleChange}
-      />
-      <Function_button text="submit" type="input"/>
-    </Form_container>
     </div>
   );
 };
