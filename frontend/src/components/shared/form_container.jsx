7037d3443e293062d02653109bb32c12bbce2f00
Aleksi Kilpilampi <aleksi.kilpilampi@tuni.fi>
2024-09-29 07:28:57 +0300

    implemented form_container and unit tests, added name attribut to text_field

--- frontend/src/components/shared/form_container.jsx
+++ frontend/src/components/shared/form_container.jsx
@@ -1,40 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Function_button from './function_button';
 const Form_container = ({children,
                           onSubmit,
                           sx = {},
-                          cardSx = {},
                           childrenSx = {} }) => {
 
     return (
-      <Box sx={{ ...defaultSx, ...sx}}>
-      {/*defaultSx is overwritten by sx if that is provided*/}
-        <Card sx = {{ ...defaultCardSx, ...cardSx}}>
-          <CardContent>
-            <form onSubmit = {onSubmit} style={{ ...defaultChildrenSx, ...childrenSx  }}>
-              {children}
-            </form>
-          </CardContent>
-        </Card>
-      </Box>
+      <Card sx = {{ ...defaultSx, ...sx}}>
+      {/*defaultCardSx is overwritten by sx if that is provided*/}
+        <CardContent>
+          <form onSubmit = {onSubmit} style={{ ...defaultChildrenSx, ...childrenSx  }}>
+            {children}
+          </form>
+        </CardContent>
+      </Card>
     );
   };
 
+
+
 const defaultSx = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',   
-  padding: 1
-
-}
-
-const defaultCardSx = {
-  width: '100%',
   maxWidth: 500,
   padding: 1
 }
@@ -42,15 +30,14 @@ const defaultCardSx = {
 const defaultChildrenSx = {
   display: 'flex',
   flexDirection: 'column',
-  gap: 50,
-  width: '100%'
+  gap: 50
 }
 
-Function_button.propTypes = {
+Form_container.propTypes = {
   children: PropTypes.node.isRequired, //mandatory
   onSubmit: PropTypes.func.isRequired, //mandatory
   sx: PropTypes.object,
-  cardSx: PropTypes.object
+  childrenSx: PropTypes.object
 }
   
 export default Form_container;
