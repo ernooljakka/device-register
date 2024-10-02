3f94708e5054c9e1ffc1d4a12137e4fd6ea68eaa
Pyry Lehtinen <47785682+Kuovi@users.noreply.github.com>
2024-10-02 14:13:32 +0300

    Implemented reusable grid table. Currently cells cant be changed after render. (#79)

---
+++ frontend/src/__tests__/grid_table_test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/';
+import GridTable from '../components/shared/grid_table';
+
+const rowData = [
+    { test_id: '1', test_name: 'Chungus McTesterdon', test_sandwich: 'BLT', dev_dreams: 'job' },
+    { test_id: '2', test_name: 'Real TestName', test_sandwich: 'Club', dev_dreams: 'thesis' },
+  ];
+  
+const columnDefs = [
+    { field: 'test_id', headerName: 'TEST ID' },
+    { field: 'test_name', headerName: 'TEST NAME' },
+    { field: 'test_sandwich', headerName: 'EAT ME' },
+    { field: 'dev_dreams', headerName: 'CRUSHED' },
+];
+
+describe('GridTable Component', () => {
+
+    test('renders the correct column headers', () => {
+        const { getByText } = render(<GridTable rowData={rowData} columnDefs={columnDefs} />);
+        expect(getByText('TEST ID')).toBeInTheDocument();
+        expect(getByText('TEST NAME')).toBeInTheDocument();
+        expect(getByText('EAT ME')).toBeInTheDocument();
+        expect(getByText('CRUSHED')).toBeInTheDocument();
+    });
+
+    test('renders the correct number of rows', () => {
+        const { getAllByRole } = render(<GridTable rowData={rowData} columnDefs={columnDefs} />);
+        const rows = getAllByRole('row');
+        expect(rows.length).toBe(4); // Take in account header and floating filter rows.
+    });
+
+    test('renders the correct data cells', () => {
+        const { getByText } = render(<GridTable rowData={rowData} columnDefs={columnDefs} />);
+        expect(getByText('Chungus McTesterdon')).toBeInTheDocument();
+        expect(getByText('BLT')).toBeInTheDocument();
+        expect(getByText('job')).toBeInTheDocument();
+
+        expect(getByText('Real TestName')).toBeInTheDocument();
+        expect(getByText('Club')).toBeInTheDocument();
+        expect(getByText('thesis')).toBeInTheDocument();
+    });
+});
--- frontend/src/components/shared/grid_table.jsx
+++ frontend/src/components/shared/grid_table.jsx
@@ -1,14 +1,40 @@
-import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
-import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
-import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional, might change when testing different styles
+import React from 'react';
+import Box from '@mui/material/Box';
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
 
-const Grid_table = ({}) => {
+const Grid_table = ({rowData, columnDefs}) => {
+  return (
+    <Box
+      sx={{
+        flexDirection: 'column',
+        alignItems: 'center',
+        mt: 1,
+        mb: 4,
+        maxWidth: 1000,
+      }}
+      className="ag-theme-quartz"
+      style={{ width: '95vw', height: '80vh' }}
+    >
+      <AgGridReact
+        rowData={rowData} 
+        columnDefs={columnDefs} 
+        defaultColDef={{ 
+          resizable: false, 
+          sortable: true, 
+          filter: true,
+          floatingFilter: true, 
+          minWidth: 100 
+        }} 
+        animateRows={true}
+        pagination={true} 
+        paginationAutoPageSize = {true}
+        paginationPageSizeSelector={false}
+        style={{ flexGrow: 1 }}
+      />
+    </Box>
+  );
+};
 
-    return (
-      <div>
-        
-      </div>
-    );
-  };
-  
-  export default Grid_table;
+export default Grid_table;
