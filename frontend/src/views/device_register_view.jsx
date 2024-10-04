2a47e39436119dca6128dacc6122bcfdee283aa4
Pyry Lehtinen <47785682+Kuovi@users.noreply.github.com>
2024-10-04 19:56:01 +0300

    Implemented Device Register View. (#90)

--- frontend/src/views/device_register_view.jsx
+++ frontend/src/views/device_register_view.jsx
@@ -1,13 +1,34 @@
-import DeviceRegisterGrid from '../components/device_register/device_register_grid'
+import React from 'react';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+import DeviceGrid from '../components/device_register/device_register_grid';
+import NavigationBar from '../components/shared/navigation_bar';
 
-
-const Device_register_view = () => {
+function Device_register_view() {
 
   return (
-    <div>
-        <DeviceRegisterGrid/>
-    </div>
+    <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%', 
+        height: '100%',
+        overflow: 'hidden',
+        textWrap: 'nowrap'
+    }}>
+        <NavigationBar/>
+        <Typography sx={{
+          fontSize: 'clamp(1.5rem, 5vw, 2.4rem)', 
+          textAlign: 'center',
+          mt: 8, 
+          mb: 3,
+        }}>
+        Device Register
+        </Typography>
+        <DeviceGrid/>
+    </Box>
   );
-};
+}
 
 export default Device_register_view;
