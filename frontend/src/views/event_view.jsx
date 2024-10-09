94b3cae0b7c28175b88927407315156247a791be
Pyry Lehtinen <47785682+Kuovi@users.noreply.github.com>
2024-10-09 16:04:50 +0300

    Implemented Event View (#105)
    * Implemented Event View and unit tests for it.
    
    * Fixed grid_table wrapping box margins to not eat the grid.

--- frontend/src/views/event_view.jsx
+++ frontend/src/views/event_view.jsx
@@ -1,12 +1,34 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import NavigationBar from '../components/shared/navigation_bar';
+import EventGrid from '../components/event_view_components/event_grid';
+import { Typography } from '@mui/material';
 
-
-const Event_view = () => {
+function Event_view() {
 
   return (
-    <div>
-      
-    </div>
+    <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%', 
+        height: '100%',
+        overflow: 'hidden',
+        textWrap: 'nowrap'
+    }}>
+        <NavigationBar/>
+        <Typography sx={{
+          fontSize: 'clamp(1.5rem, 5vw, 2.4rem)', 
+          textAlign: 'center',
+          mt: 8, 
+          mb: 3,
+        }}>
+        Event History
+        </Typography>
+        <EventGrid />
+    </Box>
   );
-};
+}
 
 export default Event_view;
